89688f6928078f67810c8bfecb3cbeefcd647728
Awcrr <billyishere@outlook.com>
2016-06-06 13:43:20 +0800

    remove the target dqn

--- dqn/agent.py
+++ dqn/agent.py
@@ -22,7 +22,7 @@ class Agent(BaseModel):
 
         self.cnn_w = {}
 
-        with tf.variable_scope('CNN-ZFNet'):
+        with tf.variable_scope('CNN'):
             # Input_Holder
             inp = tf.placeholder(config.imgDType, [None, 224, 224, 3], 'ZFNet_input')
             # CNN_l1(including pooling and normlization)
@@ -47,10 +47,7 @@ class Agent(BaseModel):
             self.cnn_l5_flat = tf.reshape(self.pool5, [config.batch_size, -1], name = 'l5_flat')
             
             # CNN_output
-            self.cnn_output, self.w['output_w'], self.w['output_b'] = fc_layer(self.cnn_l5_flat, 4096, activation = activation, w_initializer = w_initializer, b_initializer = b_initializer, name = 'output')
-            
-        return self.cnn_output
-
+            self.cnn_output, self.cnn_w['output_w'], self.cnn_w['output_b'] = fc_layer(self.cnn_l5_flat, 4096, activation = activation, w_initializer = w_initializer, b_initializer = b_initializer, name = 'output')
 
 
     def build_dqn_net(self,config):
@@ -65,14 +62,13 @@ class Agent(BaseModel):
         w_initializer = tf.truncated_normal_initializer(config.ini_mean, config.ini_stddev)
         b_initializer = tf.constant_initializer(config.bias_start)
 
-        # deep-Q-net training weights(w) and target weights(t_w)
+        # deep-Q-net training weights(w)
         self.dqn_w = {}
-        self.dqn_t_w = {}
         # Input_Holder
         inp_size = config.featureDimension + config.actionDimension
         inp = tf.placeholder(config.featureDType, [None, inp_size], 'dqn_input')
 
-        with tf.variable_scope('prediction'):
+        with tf.variable_scope('DQN'):
             # DQN_fc1
             self.dqn_l1, self.dqn_w['l1_w'], self.dqn_b['l1_b'] = fc_layer(inp, 1024, activation = activation, w_initializer = w_initializer, b_initializer = b_initializer, name = 'dqn_l1')
             
@@ -82,25 +78,6 @@ class Agent(BaseModel):
             # DQN_output
             self.dqn_l3, self.dqn_w['output_w'], self.dqn_b['output_b'] = fc_layer(self.dqn_l2, config.action_size, w_initializer = w_initializer, b_initializer = b_initializer, name = 'dqn_l3')
             self.q_action = tf.argmax(self.q, dimension = 1)
-
-        with tf.variable_scope('target'):
-            # DQN_target_fc1
-            self.dqn_t_l1, self.dqn_t_w['l1_w'], self.dqn_t_b['l1_b'] = fc_layer(inp, 1024, activation = activation, w_initializer = w_initializer, b_initializer = b_initializer, name = 'dqn_t_l1')
-
-            # DQN_target_fc2
-            self.dqn_t_l2, self.dqn_t_w['l2_w'], self.dqn_t_b['l2_b'] = fc_layer(self.dqn_t_l1, 1024, activation = activation, w_initializer = w_initializer, b_initializer = b_initializer, name = 'dqn_t_l2')
-
-            # DQN_target_output
-            self.dqn_t_l3, self.dqn_t_w['output_w'], self.dqn_t_b['output_b'] = fc_layer(self.dqn_t_output, config.action_size, w_initializer = w_initializer, b_initializer = b_initializer, name = 'dqn_t_l3')
-
-        # update weights
-        with tf.variable_scope('pred_to_target'):
-            self.dqn_t_w_inp = {}
-            self.dqn_t_w_assign_op = {}
-
-            for key in self.dqn_w.keys():
-                self.dqn_t_w_inp[key] = tf.placeholder('float32', self.dqn_t_w[key].get_shape().as_list(), name = key)
-                self.dqn_t_w_assign_op[key] = self.dqn_t_w[key].assign(self.dqn_t_w_inp[key])
 
         # optimizer
         with tf.variable_scope('dqn_optimizer'):
@@ -124,6 +101,9 @@ class Agent(BaseModel):
                         self.dqn_learning_rate_decay,
                         staircase = True))
             self.dqn_optim = tf.train.RMSPropOptimizer(self.learning_rate_op, momentum = config.dqn_momentum, epsilon = config.dqn_epsilon).minimize(self.loss)
+        
+        # DQN initialization
+        tf.initialize_all_variables().run()
 
     def record(self):
 
